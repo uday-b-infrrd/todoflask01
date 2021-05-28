c6eedd770fa26755426b2bf12e2ac90cc2faafc7
Uday B <59685147+udayb511@users.noreply.github.com>
2021-05-28 15:30:32 +0530

    using sql

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
-from os import error
-from flask import Flask,request,jsonify
-from flask_sqlalchemy import SQLAlchemy
+import MySQLdb
+from flask import Flask, json,request,jsonify,Response
+from flask_mysqldb import MySQL
+from werkzeug.datastructures import HeaderSet
+import yaml
 from datetime import datetime
-from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+# Configuration db
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST']= db['mysql_host']
+app.config['MYSQL_USER']= db['mysql_user']
+app.config['MYSQL_PASSWORD']= db['mysql_password']
+app.config['MYSQL_DB']= db['mysql_db']
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-class TodoSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Todo
-        load_instance = True
-
-todoSchema = TodoSchema()
-todoListSchema = TodoSchema(many=True)
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    tasks = Todo.query.order_by(Todo.date_created).all()
-    all_tasks = todoListSchema.dump(tasks)
-    if not tasks:
-        error = "No tasks available"
-        return jsonify(error)
+    cur = mysql.connection.cursor()
+    cur.execute("select * from todo")
+    all_tasks = cur.fetchall()
+    row_header = [decription[0] for decription in cur.description]
+    cur.close()
+    if not all_tasks:
+        return jsonify({'error' : 'No tasks available'}),404
     else:
-        return jsonify(all_tasks)
+        return jsonify(toJsonFormat(row_header,all_tasks)),200
 
 @app.route('/add', methods=['Post'])
 def add():
@@ -43,40 +35,71 @@ def add():
         return jsonify({'error': 'content should not be empty'})
     else:          
         todo_content = task_content['content']
-        new_task = Todo(content = todo_content)
         try:
-            db.session.add(new_task)
-            db.session.commit()
-            return jsonify({'message': 'Successfully task added'})
-        except:
-            return jsonify({'error' : 'There was an issue adding your task'})
+            datenow = datetime.now().date()
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT into todo(content, date_created) values(%s,%s)",(todo_content,datenow))
+            mysql.connection.commit()
+            cur.close()
+            return jsonify({'message': 'Successfully task added'}),200
+        except(MySQLdb.Error, MySQLdb.Warning) as sqlError:
+            print(sqlError)
+            return jsonify({'error' : 'There was an issue adding your task',
+                            'message': sqlError})
+
 
 @app.route('/delete/<task_id>',methods=['GET'])
 def delete(task_id):
-    task_to_delete = Todo.query.get_or_404(task_id)
     try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return jsonify({'message': 'task deleted successfully'})
-    except:
-        return jsonify({'error': 'There was a problem deleting that task...plz retry'})
+        cur = mysql.connection.cursor()
+        if(cur.execute("select * from todo where id=(%s)",(task_id))):
+            cur.execute("delete from todo where id=(%s)",(task_id))
+            mysql.connection.commit()
+            cur.close()
+            return jsonify({'message': 'task deleted successfully'}),200
+        else:
+            return jsonify({'error':'no data present with task id'}),404
+    except(MySQLdb.Error, MySQLdb.Warning)as sqlError:
+        print(sqlError)
+        return Response({'error': 'There was a problem deleting that task...plz retry',
+                        'message': sqlError})
 
 @app.route('/update/<task_id>', methods=['GET', 'POST'])
 def update(task_id):
-    task = Todo.query.get_or_404(task_id)
-
     if request.method == 'POST':
         task_content = request.get_json()
-        task.content = task_content['content']
+        content = task_content['content']
         try:
-            db.session.commit()
-            return jsonify({'message': 'task updated successfully'})
-        except:
-            return jsonify({'error': 'There was an issue updating your task'})
-
+            cur = mysql.connection.cursor()
+            if(cur.execute("select * from todo where id=(%s)",(task_id))):
+                cur.execute("update todo set content=(%s) where id=(%s)",(content,task_id))
+                mysql.connection.commit()
+                cur.close()
+                return jsonify({'message': 'task updated successfully'}),200
+            else:
+                return jsonify({'error' : 'no data present with task that id'}),404
+        except(MySQLdb.Error, MySQLdb.Warning)as sqlError:
+            return jsonify({'error': 'There was an issue updating your task','message': sqlError})
     else:
-        task_contant_to_update = todoSchema.dump(task)
-        return jsonify(task_contant_to_update)
+        try:
+            cur = mysql.connection.cursor()
+            if(cur.execute("select * from todo where id=(%s)",(task_id))):
+                cur.execute("select * from todo where id=(%s)",(task_id))
+                sql_data = cur.fetchall()
+                row_header = [decription[0] for decription in cur.description]
+                mysql.connection.commit()
+                cur.close()
+                return jsonify(toJsonFormat(row_header,sql_data)),200
+            else:
+                return jsonify({'error' : 'no data present with task that id'}),404
+        except(MySQLdb.Error, MySQLdb.Warning)as sqlError:
+            return jsonify({'error': 'There was an issue updating your task','message': sqlError})
+
+def toJsonFormat(row_header,sql_data):
+    json_data=[]
+    for row in sql_data:
+        json_data.append(dict(zip(row_header,row)))
+    return json_data
 
 if __name__ == "__main__":
     app.run(debug=True)
